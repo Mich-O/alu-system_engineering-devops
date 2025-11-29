1ff5f6e0af12926b8faafcb88455413f10ffef09
Mich-O <m.odhiambo@alustudent.com>
2025-11-29 13:40:23 +0200

    Update

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,9 @@
 # This Puppet manifest restores a missing WordPress file that causes Apache to throw a 500 error
 
 file { '/var/www/html/wp-settings.php':
-  ensure  => file,
-  source  => '/usr/share/wordpress/wp-settings.php',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
+  ensure => file,
+  source => '/usr/share/wordpress/wp-settings.php',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0644',
 }
-
